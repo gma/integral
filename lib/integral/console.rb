bbd8eff0a34abd54abe7e697b0b60a282f6f19d5
Graham Ashton <graham@effectif.com>
2008-11-11 16:52:29 +0000

    Added create_test_run_from_live_versions for bootstrapping a fresh install.

--- lib/integral/console.rb
+++ lib/integral/console.rb
@@ -1,3 +1,15 @@
 # Loaded by script/console.
 
 Integral::Database.connect
+
+# You should only use this function when you first install integral, to give
+# yourself a successful test run for your existing applications. You should
+# use the app:add thor task to add all relevant applications before you
+# run it.
+#
+def create_test_run_from_live_versions(success)
+  run = TestRun.new
+  run.application_versions << ApplicationVersion.check_current_versions(:live)
+  run.passed = success
+  run.save!
+end
