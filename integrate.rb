a84e765030c4f2c579b02fadc8d4d363d09c2920
Graham Ashton <graham@effectif.com>
2008-11-11 14:32:04 +0000

    Added default test script (to be replaced by the user).

---
+++ integrate.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+script = File.expand_path(File.join(File.dirname(__FILE__), "integrate.rb"))
+
+puts "###"
+puts "### Edit integrate.rb so that it launches your test suite!"
+puts "###"
+puts "### You'll find it here:"
+puts "### #{script}"
+puts "###"
+puts "### If your script exits with a return value of 0 your tests are"
+puts "### considered to have passed. If non-zero, they'll be recorded"
+puts "### as a big fat FAIL."
+puts "###"
+
+sleep 5
